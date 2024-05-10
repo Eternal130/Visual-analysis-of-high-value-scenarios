f9a5a8e3d3c00dff1bd4bb8a6d28f3cdb6a1b1df
Eternal130 <111716703+Eternal130@users.noreply.github.com>
2024-05-10 21:54:03 +0800

    格式化文件

--- myproject/index/views.py
+++ myproject/index/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
 import os
-from django.http import FileResponse
+
 from django.http import FileResponse, HttpResponseNotFound
-ab_Path=r"D:\web\myproject"+'\\'
+from django.shortcuts import render
+
+
+# ab_Path=r"D:\web\myproject"+'\\'
 def index(request):
-    return render(request,ab_Path+r'templates\T.html')
+    return render(request, r'T.html')
+
 
 def get_geojson(request, filename):
     # 构建完整的文件路径
     filepath = os.path.join(r'D:\web\myproject\map', filename)
     # 检查文件是否存在
-    
+
     if os.path.exists(filepath):
         # 如果文件存在，返回该文件作为响应
         return FileResponse(open(filepath, 'rb'), content_type='application/json')
